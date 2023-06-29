9c7cc8d2b9cd0e1bb8d00806c65ee15f545c3f1c
Kiran Shila <kshila@caltech.edu>
2023-06-29 11:25:25 -0700

    Add helper scripts

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Build T0
+cd t0
+if ! command -v cargo &> /dev/null
+then
+    echo "cargo could not be found, is the rust toolchain installed?"
+    exit
+else
+    cargo build --release
+fi
+cd ..
+
+# Build T2
+cd t2
+if ! command -v poetry &> /dev/null
+then
+    echo "poetry could not be found, is it installed?"
+    exit
+else
+    poetry install
+fi
+cd ..
